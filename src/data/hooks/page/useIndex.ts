a88d687556267260e2cd41c5c550440ba91abb4b
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-05 15:16:17 -0300

    adicionando email setemail

--- src/data/hooks/page/useIndex.ts
+++ src/data/hooks/page/useIndex.ts
@@ -5,5 +5,6 @@ import { AxiosError } from "axios";
 
 export function useIndex() {
   const [Pet__List, setPet__List] = useState<Pet[]>([]),
-    [Selected__Pet, setSelected__Pet] = useState<Pet | null>(null);
+    [Selected__Pet, setSelected__Pet] = useState<Pet | null>(null),
+    [email, setEmail] = useState("");
 }
