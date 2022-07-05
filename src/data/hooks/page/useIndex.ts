32cebda4b6c5695a468cb5e004ea7509a9873e78
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-05 15:01:30 -0300

    adicionando from

--- src/data/hooks/page/useIndex.ts
+++ src/data/hooks/page/useIndex.ts
@@ -1,4 +1,4 @@
 import { useState, useEffect } from "react";
 import { Pet } from "../../@types/Pet";
 import { ApiService } from "../../services/ApiService";
-import { AxiosError }
+import { AxiosError } from
