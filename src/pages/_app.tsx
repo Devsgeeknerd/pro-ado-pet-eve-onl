53e52a4be0f63eb2d5a6e76291b4204ee7bfc966
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-06 14:37:26 -0300

    adicionando header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,3 +2,4 @@ import "../ui/styles/global.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import theme from "../ui/themes/theme";
+import Header from "../ui/components/Header/Header";
