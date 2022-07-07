a58c467009a62c657e37e81d7d5cd448f5e6ca5c
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-07 11:53:11 -0300

    adicionando grid container spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,6 +38,7 @@ const Home: NextPage = () => {
     </>
     <List pets={Pet__List} When__Selected={(pet) => setSelected__Pet(pet)} />
     <Dialog open={Selected__Pet !== null} fullWidth PaperProps={{ sx: { p: 5 } }} onClose={() => setlected__Pet(null)}>
+      <Grid container spacing={2}>
     </Dialog>
   );
 };
