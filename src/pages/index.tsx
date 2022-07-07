381598f677217a0aeebeb3b9831e19f1573dc083
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-07 09:56:59 -0300

    adicionando grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
 import type { NextPage } from "next";
 import Title from "../ui/components/Title";
 import List from "../ui/components/List";
-import { Dialog, TextField } from "@mui/material";
+import { Dialog, TextField, Grid } from "@mui/material";
