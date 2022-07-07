7e36c4bdf873e9769030cbf478c47b891472c54e
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-07 10:49:37 -0300

    adicionando useindex

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Title from "../ui/components/Title";
 import List from "../ui/components/List";
+import { useIndex } from "../data/hooks/page/useIndex";
 import {
   Dialog,
   TextField,
