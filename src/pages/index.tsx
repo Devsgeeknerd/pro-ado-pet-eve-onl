41c4e0bc4a99d2d0059dc9ec2d6c53be04eb3e78
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-08 14:14:45 -0300

    fechando o button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,7 @@ const Home: NextPage = () => {
           </Grid>
         </Grid>
         <DialogActions sx={{ mt: 5 }}>
-          <Button color={"secondary"}
+          <Button color={"secondary"} onClick={}></Button>
         </DialogActions>
       </Dialog>
     </>
