1ddd65533242a17960c5b946d4402762604635dd
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-08 14:07:27 -0300

    adicionando onchange

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,6 +58,7 @@ const Home: NextPage = () => {
               type={"number"}
               fullWidth
               value={value}
+              onChange={(e) => setValue(e.target.value)}
             />
           </Grid>
         </Grid>
