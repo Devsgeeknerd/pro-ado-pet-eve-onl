26a56d9852b9e7363097f6c237ab31391895195b
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-08 14:13:35 -0300

    adicionando color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,9 @@ const Home: NextPage = () => {
             />
           </Grid>
         </Grid>
-        <DialogActions sx={{ mt: 5 }}></DialogActions>
+        <DialogActions sx={{ mt: 5 }}>
+          <Button color={"secondary"}
+        </DialogActions>
       </Dialog>
     </>
   );
