b3d18b06e2eb9b980b5878f301d7bf01bdf7552d
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-08 15:27:24 -0300

    adicionando src

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Header__Container, Logo } from "./Header.style";
 function Header() {
   return (
     <Header__Container>
-      <Logo />
+      <Logo src="/images/logo.svg" alt="Adopt a Pet" />
     </Header__Container>
   );
 }
