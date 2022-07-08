54da934e948ab73e7c9644d932be56b91ff9a864
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-08 15:06:18 -0300

    adicionando message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ const Home: NextPage = () => {
           </Button>
         </DialogActions>
       </Dialog>
-      <Snackbar open={message.length > 0} message={} />
+      <Snackbar open={message.length > 0} message={message} />
     </>
   );
 };
