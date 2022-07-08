888dddcdb3c39aebd0312128f089543165ef0c7b
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-08 14:58:42 -0300

    adding confirm adoption

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,7 +66,10 @@ const Home: NextPage = () => {
           <Button color={"secondary"} onClick={() => setSelected__Pet(null)}>
             Cancel
           </Button>
-          <Button variant={"contained"} onClick={() => adopt}></Button>
+          <Button variant={"contained"} onClick={() => adopt}>
+            {" "}
+            Confirm Adoption
+          </Button>
         </DialogActions>
       </Dialog>
     </>
