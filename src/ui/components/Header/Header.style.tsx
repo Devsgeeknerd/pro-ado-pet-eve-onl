88f7bd0282cc4a8b68990a2993901b4071b4cabd
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-09 11:24:28 -0300

    adicionando padding

--- src/ui/components/Header/Header.style.tsx
+++ src/ui/components/Header/Header.style.tsx
@@ -1,5 +1,6 @@
 import { styled } from "@mui/material";
 
 export const Header__Container = styled("header")`
+  padding: ${({ theme }) => theme.spacing(6)};
   border-bottom: 1px solid #f0f0f0;
 `;
