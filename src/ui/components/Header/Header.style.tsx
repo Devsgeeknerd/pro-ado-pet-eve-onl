c5b115d664c582f01cd9b68c6bdb866f0cbd1241
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-09 11:22:19 -0300

    adicionando border bottom

--- src/ui/components/Header/Header.style.tsx
+++ src/ui/components/Header/Header.style.tsx
@@ -1,3 +1,5 @@
 import { styled } from "@mui/material";
 
-export const Header__Container = styled("header");
+export const Header__Container = styled("header")`
+  border-bottom: 1px solid #f0f0f0;
+`;
