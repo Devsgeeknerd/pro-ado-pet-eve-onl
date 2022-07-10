d962f13e0cdf400688b8b0bc830be6193cd89da9
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-10 15:29:01 -0300

    adicionando contained

--- src/ui/components/List/List.tsx
+++ src/ui/components/List/List.tsx
@@ -27,7 +27,7 @@ function List(props: ListProps) {
             <Information__Description>
               {TextService.Limit__Text(pet.history, Maximum__Size)}
             </Information__Description>
-            <Button variant={}></Button>
+            <Button variant={"contained"}></Button>
           </Information>
         </List__Item>
       ))}
