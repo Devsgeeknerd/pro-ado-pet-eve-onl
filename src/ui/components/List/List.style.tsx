ee64856e9f5e4c75eca908735b6ad3d268881dc2
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-11 12:28:24 -0300

    adicionando md

--- src/ui/components/List/List.style.tsx
+++ src/ui/components/List/List.style.tsx
@@ -14,5 +14,5 @@ export const List__Item = styled("li")`
   grid-gap: ${({ theme }) => theme.spacing(5)};
 
   /* BREAKPOINTS */
-  ${({ theme }) => theme.breakpoints.down
+  ${({ theme }) => theme.breakpoints.down("md")
 `;
