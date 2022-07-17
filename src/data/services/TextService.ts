cd42f7c45b2f4f486d9829f2dadfe9952fa24857
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-17 17:53:03 -0300

    deletando text services

--- src/data/services/TextService.ts
+++
@@ -1,8 +0,0 @@
-export const TextService = {
-  Limit__Text(text: string, maximum__size: number): string {
-    if (text.length < maximum__size) {
-      return text;
-    }
-    return text.slice(0, maximum__size) + "...";
-  },
-};
