c6a75583e2010fe53f741a09774d1e36975642de
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-17 17:50:48 -0300

    deletando header

--- src/ui/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Header__Container, Logo } from "./Header.style";
-
-function Header() {
-  return (
-    <Header__Container>
-      <Logo src="/images/logo.svg" alt="Adopt a Pet" />
-    </Header__Container>
-  );
-}
