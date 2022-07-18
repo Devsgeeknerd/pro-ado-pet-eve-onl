d1c115273c4074cbddb56a966f9dd4a271281c5d
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-18 11:45:45 -0300

    adicionando header

--- Front-end/src/pages/_app.tsx
+++ Front-end/src/pages/_app.tsx
@@ -2,4 +2,4 @@ import "../ui/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import theme from "../ui/themes/theme";
-import Header from "../ui/components/Header";
+import Header from "../ui/components/Header/Header";
