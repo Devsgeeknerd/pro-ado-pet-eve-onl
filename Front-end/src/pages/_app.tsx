bdd32cb886cc91ac8eeb0e4f1dfdff9a0c3891fd
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-18 11:38:29 -0300

    adicionando components

--- Front-end/src/pages/_app.tsx
+++ Front-end/src/pages/_app.tsx
@@ -2,4 +2,4 @@ import "../ui/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import theme from "../ui/themes/theme";
-import Header from "../ui";
+import Header from "../ui/components";
