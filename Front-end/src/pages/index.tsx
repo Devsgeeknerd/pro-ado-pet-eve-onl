b1a4f8d7e5c488dbf9c5bfd41c95044dbc7bb7be
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-19 16:29:25 -0300

    adicionando selected pet

--- Front-end/src/pages/index.tsx
+++ Front-end/src/pages/index.tsx
@@ -12,6 +12,6 @@ import {
 } from "@mui/material";
 
 const Home: NextPage = () => {
-  const { Pet__List } = useIndex();
+  const { Pet__List, Selected__Pet } = useIndex();
 };
 export default Home;
