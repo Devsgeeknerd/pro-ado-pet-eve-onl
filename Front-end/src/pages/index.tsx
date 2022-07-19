1bf576f66dccde8021274990e47f8cec256a5c09
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-19 16:17:15 -0300

    adicionando from

--- Front-end/src/pages/index.tsx
+++ Front-end/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Title from "../ui/components/Title/Title";
 import List from "../ui/components/List/List";
-import
+import {} from "";
 import {
   Dialog,
   TextField,
