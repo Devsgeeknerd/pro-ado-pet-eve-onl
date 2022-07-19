729d2954cbf35aebd9d47c0a0cc32cbeddbb50b8
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-19 16:30:50 -0300

    adicionando email

--- Front-end/src/pages/index.tsx
+++ Front-end/src/pages/index.tsx
@@ -12,6 +12,6 @@ import {
 } from "@mui/material";
 
 const Home: NextPage = () => {
-  const { Pet__List, Selected__Pet, setSelected__Pet } = useIndex();
+  const { Pet__List, Selected__Pet, setSelected__Pet, email } = useIndex();
 };
 export default Home;
