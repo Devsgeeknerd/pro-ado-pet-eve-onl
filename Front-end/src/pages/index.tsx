27e5cfd1c623406e6d60861aabc1e8249d6ac43a
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-20 16:35:56 -0300

    adicionando selected pet

--- Front-end/src/pages/index.tsx
+++ Front-end/src/pages/index.tsx
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
         }
       />
       <List pets={Pet__List} When__Selected={(pet) => setSelected__Pet(pet)} />
-      <Dialog open={}></Dialog>
+      <Dialog open={Selected__Pet}></Dialog>
     </>
   );
 };
