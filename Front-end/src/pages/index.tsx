d9d4cb622df9fe2255877c1706f847e34f7f8bdb
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-21 11:20:51 -0300

    adicionando adopt

--- Front-end/src/pages/index.tsx
+++ Front-end/src/pages/index.tsx
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
           <Button color={"secondary"} onClick={() => setSelected__Pet(null)}>
             Cancelar
           </Button>
-          <Button variant={"contained"} onClick={() =>()}></Button>
+          <Button variant={"contained"} onClick={() => adopt()}></Button>
         </DialogActions>
       </Dialog>
       <Snackbar />
