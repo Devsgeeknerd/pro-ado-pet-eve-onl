e6f97202b5e3d6fe94820c62158211ab381cad8e
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-22 14:36:54 -0300

    adicionando logo

--- Front-end/src/ui/components/Header/Header.tsx
+++ Front-end/src/ui/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Header__Container, Logo } from "./styles";
 function Header() {
   return (
     <Header__Container>
-      <Logo src="./images" alt="Adote um Pet" />
+      <Logo src="./images/logo.svg" alt="Adote um Pet" />
     </Header__Container>
   );
 }
