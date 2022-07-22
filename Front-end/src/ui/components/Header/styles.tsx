19246031600aa2b9f209c673a0ad725f82bf72d5
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-22 15:11:27 -0300

    adicionando border-bottom

--- Front-end/src/ui/components/Header/styles.tsx
+++ Front-end/src/ui/components/Header/styles.tsx
@@ -2,5 +2,6 @@ import { styled } from "@mui/material";
 
 export const Header__Container = styled("header")`
   padding: ${({ theme }) => theme.spacing(6)};
+  border-bottom: 1px solid #f0f0f0;
   display: flex;
 `;
