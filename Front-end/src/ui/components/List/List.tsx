6fbc2806059ade2bad7e3f4fa8940783923b5a38
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-23 10:30:18 -0300

    adicionando services

--- Front-end/src/ui/components/List/List.tsx
+++ Front-end/src/ui/components/List/List.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@mui/material";
 import { Pet } from "../../../data/@types/Pet";
-import { TextService } from "../../../data";
+import { TextService } from "../../../data/services";
 import {
   List__Container,
   List__Item,
