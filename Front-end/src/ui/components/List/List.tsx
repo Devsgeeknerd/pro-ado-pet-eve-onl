6ed84ed709c21bbfd543e35fb829f862943a538c
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-25 11:48:39 -0300

    adicionando full width

--- Front-end/src/ui/components/List/List.tsx
+++ Front-end/src/ui/components/List/List.tsx
@@ -27,7 +27,7 @@ function List(props: ListProps) {
             <Information__Description>
               {TextService.Limit__Text(pet.history, maximum__size)}
             </Information__Description>
-            <Button variant={"contained"}>
+            <Button variant={"contained"} fullWidth>
       ))}
     </List__Container>
   );
