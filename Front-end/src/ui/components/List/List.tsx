92c0726b18f04376e11090cbf21bd0b9565acc20
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-25 11:47:59 -0300

    adicionando contained

--- Front-end/src/ui/components/List/List.tsx
+++ Front-end/src/ui/components/List/List.tsx
@@ -27,7 +27,7 @@ function List(props: ListProps) {
             <Information__Description>
               {TextService.Limit__Text(pet.history, maximum__size)}
             </Information__Description>
-            <Button variant={}>
+            <Button variant={"contained"}>
       ))}
     </List__Container>
   );
