983180f95d9f76a5596db4f9143ec11d405a0578
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-25 11:06:56 -0300

    adicionando name

--- Front-end/src/ui/components/List/List.tsx
+++ Front-end/src/ui/components/List/List.tsx
@@ -21,7 +21,7 @@ function List(props: ListProps) {
     <List__Container>
       {props.pets.map((pet) => (
         <List__Item key={pet.id}>
-          <List__Photo src={pet.photo} alt={pet} />
+          <List__Photo src={pet.photo} alt={pet.name} />
       ))}
     </List__Container>
   );
