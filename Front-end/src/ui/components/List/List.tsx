242c882fa34ad09e73864c465b8c3b8191b6fcf2
Paulo Vanderley <vanderley.1109@gmail.com>
2022-07-25 11:52:51 -0300

    adicionando pet

--- Front-end/src/ui/components/List/List.tsx
+++ Front-end/src/ui/components/List/List.tsx
@@ -27,7 +27,7 @@ function List(props: ListProps) {
             <Information__Description>
               {TextService.Limit__Text(pet.history, maximum__size)}
             </Information__Description>
-            <Button variant={"contained"} fullWidth onClick={() => props.When__Selected()}>
+            <Button variant={"contained"} fullWidth onClick={() => props.When__Selected(Pet)}>
       ))}
     </List__Container>
   );
