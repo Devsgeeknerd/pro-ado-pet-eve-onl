dd42752e661531e1b64f175a7a168a3a80ab969e
Paulo <vanderley.1109@gmail.com>
2024-01-23 16:23:48 -0300

    atualizando conteudo

--- Front-end/src/ui/themes/themes.ts
+++ Front-end/src/ui/themes/themes.ts
@@ -1,24 +1,33 @@
+// Importa a função createTheme do pacote @mui/material.
 import { createTheme } from "@mui/material";
 
+// Cria um tema personalizado utilizando a função createTheme.
 const theme = createTheme({
+  // Define a paleta de cores do tema
   palette: {
+    // Define a cor principal (primária) com o código hexadecimal #ae0fea.
     primary: {
       main: "#ae0fea",
     },
+    // Define a cor secundária com o código hexadecimal #c5c5c5.
     secondary: {
       main: "#c5c5c5",
     },
+    // Define cores de texto primárias e secundárias.
     text: {
       primary: "#293845",
       secondary: "#9eadba",
     },
   },
+  // Define configurações de tipografia, como a fonte utilizada.
   typography: {
     fontFamily: "Roboto, sans-serif",
   },
+  // Define propriedades de formato, como a borda de componentes.
   shape: {
     borderRadius: "4px",
   },
+  // Define estilos específicos para componentes MuiButton, MuiPaper, MuiTextField, MuiTableHead e MuiTableCell.
   components: {
     MuiButton: {
       styleOverrides: {
@@ -62,4 +71,5 @@ const theme = createTheme({
   },
 });
 
+// Exporta o tema criado para que possa ser utilizado em outros arquivos.
 export default theme;
